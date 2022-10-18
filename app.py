253688be58018d1cb9457849a85c42a9ca41a2e9
Annie Treasa <anskpally@gmail.com>
2022-10-18 17:36:54 +0530

    Added UI

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/cal')
+def cal():
+    return render_template("cal.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5000)
