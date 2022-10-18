4257ae1c2487acb336324ee6788230657ba564d9
Annie Treasa <anskpally@gmail.com>
2022-10-18 18:00:19 +0530

    Creating add event option

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+events =[
+    {
+        'title' : 'TestEvent',
+        'start' : '2022-10-18',
+        'end': '',
+        'url': ''
+    },
+    {
+        'title' : 'TestEvent2',
+        'start' : '2022-10-18',
+        'end': '2022-12-10',
+        'url': ''
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('add.html')
 
 @app.route('/cal')
 def cal():
